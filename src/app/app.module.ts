6b7b78176a2299ba2dfeebd16c22dc88aa831ee6
Leslie Gayard <38896383+LesGayard@users.noreply.github.com>
2024-05-26 15:32:07 +0200

    added Calendar before ng bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,19 @@ import {NgModule} from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-
-
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+// @ts-ignore
+import { FlatpickrModule } from 'angularx-flatpickr';
+//import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
   ],
   imports: [
+    CommonModule,
+    FormsModule,
+    //NgbModalModule,
+    FlatpickrModule.forRoot(),
     BrowserAnimationsModule,
     CalendarModule.forRoot({
       provide: DateAdapter,
